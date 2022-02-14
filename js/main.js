31f5fe3419d4c1c48496e93ccbbfccd790b091d3
Alisher959 <85029201+Alisher959@users.noreply.github.com>
2022-02-14 21:33:25 +0500

    initial cm

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin:10,
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2800,
+    });
+  });
+  new Typed('#typed',{
+    strings : ['VIDEO KURSGA BUYURTMA BERISH','VLOG.UZ-INNOVATSION VIDEO KURSLAR'],
+    typeSpeed : 60,
+    delaySpeed : 90,
+    loop : true
+  });
